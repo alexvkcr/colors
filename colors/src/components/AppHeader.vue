6042a9c11f13b17537d19ce9ee85197ff8218a50
Diaz Muñoz <alexvkcr@gmail.com>
2020-01-28 15:02:16 +0100

    feat: normalization vuetify

--- colors/src/components/AppHeader.vue
+++ colors/src/components/AppHeader.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="AppHeader">
     <h1>Learn all the colors!</h1>
+    <v-tabs
+      fixed-tabs
+      color="#2E6C61"
+    >
+      <v-tab>
+        Presentación
+      </v-tab>
+      <v-tab>
+      Option
+    </v-tab>
+    <v-tab>
+      Another Selection
+    </v-tab>
+    <v-tab>
+      Items
+    </v-tab>
+    <v-tab>
+      Another Screen
+    </v-tab>
+    </v-tabs>
   </div>
 </template>
 
@@ -11,4 +31,8 @@ export default {
 </script>
 
 <style scoped>
+h1{
+    color: #2E6C61;
+    text-align: center;
+}
 </style>
--- colors/src/components/Presentation.vue
+++ colors/src/components/Presentation.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="Presentation">
+    Hello!
   </div>
 </template>
 
