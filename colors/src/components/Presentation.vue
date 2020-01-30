f647e920b0c55fa7cb727e2f9c716c7468dd16d7
Diaz Muñoz <alexvkcr@gmail.com>
2020-01-30 13:43:27 +0100

    feat:added change language front feature

--- colors/src/components/Presentation.vue
+++ colors/src/components/Presentation.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="Presentation">
     <h2>{{ $t('welcome') }}</h2>
+    <div class="select">
+      <p>{{$t('change')}}</p>
+      <select>
+        <option value="en">{{$t('en')}}</option>
+        <option value="es">{{$t('es')}}</option>
+      </select>
+    </div>
     <h3>{{ $t('learn') }}</h3>
   </div>
 </template>
@@ -15,19 +22,32 @@ export default {
 {
   "es": {
     "welcome": "¡Bienvenidos a GetColors!",
-    "learn": "Aquí puedes aprender los nombres de TODOS los colores existentes"
+    "learn": "Aquí puedes aprender los nombres de TODOS los colores existentes",
+    "change": "Cambiar idioma",
+    "en": "Inglés",
+    "es": "Español"
   },
   "en": {
     "welcome": "Welcome to GetColors!",
-    "learn": "Here you can learn the name of ALL the existing colors"
+    "learn": "Here you can learn the name of ALL the existing colors",
+    "change": "Switch language",
+    "en": "English",
+    "es": "Spanish"
   }
 }
 </i18n>
 
 <style scoped>
+*{
+    color: #2e586c;
+}
 h2,h3{
     margin: 30px 0;
-    color: #2e586c;
     text-align: center;
+}
+.select{
+  position: relative;
+  bottom: 30px;
+  left: 90%;
 }
 </style>
