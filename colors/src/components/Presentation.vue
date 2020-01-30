8c1fe5ab30d32dbc60d23a8fee005b21b56bbee8
Diaz Muñoz <alexvkcr@gmail.com>
2020-01-30 13:33:30 +0100

    feat: introductory text

--- colors/src/components/Presentation.vue
+++ colors/src/components/Presentation.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="Presentation">
-    Hello!
+    <h2>{{ $t('welcome') }}</h2>
+    <h3>{{ $t('learn') }}</h3>
   </div>
 </template>
 
@@ -10,5 +11,23 @@ export default {
 }
 </script>
 
+<i18n>
+{
+  "es": {
+    "welcome": "¡Bienvenidos a GetColors!",
+    "learn": "Aquí puedes aprender los nombres de TODOS los colores existentes"
+  },
+  "en": {
+    "welcome": "Welcome to GetColors!",
+    "learn": "Here you can learn the name of ALL the existing colors"
+  }
+}
+</i18n>
+
 <style scoped>
+h2,h3{
+    margin: 30px 0;
+    color: #2e586c;
+    text-align: center;
+}
 </style>
