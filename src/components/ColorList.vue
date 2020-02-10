79bacb5e0b388fc809ac09020b2a002f3c51b7a3
Diaz Muñoz <alexvkcr@gmail.com>
2020-02-10 16:48:32 +0100

    feat: new component implementation

---
+++ src/components/ColorList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="ColorList">
+      {{ propColorList }}
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ColorList',
+  props: {
+      'propColorList': Array
+  }
+}
+</script>
+
+<style scoped>
+</style>
