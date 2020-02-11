f60d3c69688bd8b74b1586cfacb4cb94514c31f8
Diaz Muñoz <alexvkcr@gmail.com>
2020-02-11 15:42:54 +0100

    feat: colors displaying color :)

--- src/components/ColorList.vue
+++ src/components/ColorList.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="ColorList">
-      <p v-for="color in propColorList" :key="color.codes" >{{color[$i18n.locale]}}</p>
+      <div>Nombre | Color</div>
+      <div v-for="color in propColorList" :key="color.codes" class="colorBox" >
+          {{color[$i18n.locale]}} <div class="colorDisplay" v-bind:style="{ backgroundColor: '#'+color.codes[0]}"> </div>
+      </div>
       {{this.$i18n.locale}}
   </div>
 </template>
@@ -16,4 +19,12 @@ export default {
 </script>
 
 <style scoped>
+.colorBox{
+    display: flex;
+}
+.colorDisplay{
+    height: 15px;
+    width: 50px;
+    background-color: black;
+}
 </style>
