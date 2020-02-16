1b44a86a1bbfd029d9199e86f541c73636ab2ce7
Alejandro Díaz <alexvkcr@gmail.com>
2020-02-16 20:43:13 +0100

    ongoing feat/dynamic component addition

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="Play">
-    Play!
+    <component :is="dynamicComponent"></component> 
   </div>
 </template>
 
 <script>
+import ColorNames from './ColorNames.vue';
+import NameColors from './NameColors.vue';
+
 export default {
-  name: 'Play'
+  name: 'Play',   
+  props: {     
+    component: Boolean   
+    },    
+    computed: {     
+      dynamicComponent() {       
+      if(component) {         
+        return 'name-colors';       
+      } else {         
+        return 'color-names';       
+        }    
+      }   
+    } 
 }
 </script>
 
