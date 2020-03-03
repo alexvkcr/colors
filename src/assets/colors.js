c9412b2eff4f3926f04748865d96b3c1059ba054
Diaz Muñoz <alexvkcr@gmail.com>
2020-03-03 13:41:07 +0100

    feat/new colors&flag antirepeating

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -4,56 +4,86 @@ export default {
         es: 'Sable',
         en: 'Sable',
         es_tip: 'Negro heráldico',
-        en_tip: 'Heraldic black'
+        en_tip: 'Heraldic black',
+        available_in_es: true,
+        available_in_en: true,
+        available_in_es: true,
+        available_in_en: true
     },
     cordoban: {
         codes: ['42352d'],
         es: 'Cordobán',
         en: 'Cordovan',
         es_tip: 'Negro pardo cuero',
-        en_tip: 'Leather dark brown'
+        en_tip: 'Leather dark brown',
+        available_in_es: true,
+        available_in_en: true,
+        available_in_es: true,
+        available_in_en: true,
+        available_in_es: true,
+        available_in_en: true,
+        available_in_es: true,
+        available_in_en: true,
+        available_in_es: true,
+        available_in_en: true,
+        available_in_es: true,
+        available_in_en: true,
+        available_in_es: true,
+        available_in_en: true
     },
     umbra: {
         codes: ['635147'],
         es: 'Umbra',
         en: 'Umber',
         es_tip: 'Marrón rojizo',
-        en_tip: 'Reddish-brown '
+        en_tip: 'Reddish-brown ',
+        available_in_es: true,
+        available_in_en: true
     },
     bistre: {
         codes: ['967117'],
         es: 'Bistre',
         en: 'Bistre',
         es_tip: 'Marrón amarillento',
-        en_tip: 'Yellowish brown'
+        en_tip: 'Yellowish brown',
+        available_in_es: true,
+        available_in_en: true
     },
     grisPayne: {
         codes: ['536878'],
         es: 'Gris Payne',
         en: "Payne's grey",
         es_tip: 'Gris azulado',
-        en_tip: 'Bluish grey'
+        en_tip: 'Bluish grey',
+        available_in_es: true,
+        available_in_en: true
     },
     grisPayneOscuro: {
         codes: ['36454F'],
         es: 'Payne oscuro',
         en: "Dark Payne's",
         es_tip: 'Gris oscuro azulado',
-        en_tip: 'Dark bluish grey'
+        en_tip: 'Dark bluish grey',
+        available_in_es: true,
+        available_in_en: true
     },
     grisFeldgrau: {
         codes: ['59646A'],
         es: 'Feldgrau',
         en: 'Feldgrau',
         es_tip: 'Gris campaña',
-        en_tip: 'Field Grey'
+        en_tip: 'Field Grey',
+        available_in_es: true,
+        available_in_en: true
     },
     sinople: {
         codes: ['008f4c'],
         es: 'Sinople',
         en: 'Sinople',
         es_tip: 'Verde heráldico',
-        en_tip: 'Heraldic green'
+        en_tip: 'Heraldic green',
+        available_in_es: true,
+        available_in_en: true
     },
     verdeMalaquito: {
         codes: ['1f533f', '12774f'],
@@ -61,215 +91,314 @@ export default {
         en: 'Malachite',
         es_tip: 'Verde malaquito',
         en_tip: 'Malachite green',
+        ,
+        available_in_es: true,
+        available_in_en: true
     },
     jade: {
         codes: ['08a86b'],
         es: 'Jade',
         en: 'Jade',
         es_tip: 'Verde jade',
-        en_tip: 'Jade green'
+        en_tip: 'Jade green',
+        available_in_es: true,
+        available_in_en: true
     },
     esmeralda: {
         codes: ['009975'],
         es: 'Esmeralda',
         en: 'Emerald',
         es_tip: 'Verde esmeralda',
-        en_tip: 'Emerald green'
+        en_tip: 'Emerald green',
+        available_in_es: true,
+        available_in_en: true
     },
     viridian: {
         codes: ['007F66'],
         es: 'Viridián',
         en: 'Viridian',
         es_tip: 'Verde viridián',
-        en_tip: 'Viridián green'
+        en_tip: 'Viridián green',
+        available_in_es: true,
+        available_in_en: true
     },
     veronese: {
         codes: ['009D7A'],
         es: 'Veronés',
         en: 'Paolo Veronese',
         es_tip: 'Verde Paolo Veronese',
-        en_tip: 'Paolo Veronese green'
+        en_tip: 'Paolo Veronese green',
+        available_in_es: true,
+        available_in_en: true
     },
     cartujo: {
         codes: ['BEB72D'],
         es: 'Cartujo',
         en: 'Chartreuse ',
         es_tip: 'Verde cartujo',
-        en_tip: 'Chartreuse green'
+        en_tip: 'Chartreuse green',
+        available_in_es: true,
+        available_in_en: true
     },
     gualda: {
         codes: ['FABD00'],
         es: 'Gualdo',
         en: 'Goldtone',
         es_tip: 'Amarillo heráldico, de la bandera de españa',
-        en_tip: "Heraldic yellow (Spanish flag)"
+        en_tip: "Heraldic yellow (Spanish flag)",
+        available_in_es: true,
+        available_in_en: true
     },
     hansa: {
         codes: ['FFDD00'],
         es: 'Hansa',
         en: 'Arylide',
         es_tip: 'Amarillo Hansa o monoazo',
-        en_tip: 'Arylide or Hansa yellow'
+        en_tip: 'Arylide or Hansa yellow',
+        available_in_es: true,
+        available_in_en: true
     },
     hansaMedio: {
         codes: ['FFCD00'],
         es: 'Hansa medio',
         en: 'Medium arylide',
         es_tip: 'Amarillo Hansa',
-        en_tip: 'Medium hansa yellow'
+        en_tip: 'Medium hansa yellow',
+        available_in_es: true,
+        available_in_en: true
     },
     hansaOscuro: {
         codes: ['FFB000'],
         es: 'Hansa oscuro',
         en: 'Dark arylide',
         es_tip: 'Amarillo Hansa oscuro',
-        en_tip: 'Dark hansa yellow'
+        en_tip: 'Dark hansa yellow',
+        available_in_es: true,
+        available_in_en: true
     },
     ambar: {
         codes: ['E49E43'],
         es: 'Ámbar',
         en: 'Amber',
         es_tip: 'Amarillo ámbar',
-        en_tip: 'Amber yellow'
+        en_tip: 'Amber yellow',
+        available_in_es: true,
+        available_in_en: true
     },
     durazno: {
         codes: ['FFDAB9'],
         es: 'Durazno',
         en: 'Peach',
         es_tip: 'Color melocotón',
-        en_tip: 'Peach color'
+        en_tip: 'Peach color',
+        available_in_es: true,
+        available_in_en: true
     },
     gamuza: {
         codes: ['E8BD85'],
         es: 'Gamuza',
         en: 'Chamois',
         es_tip: 'Naranja claro, saturación moderada',
-        en_tip: 'Light orange, light saturation'
+        en_tip: 'Light orange, light saturation',
+        available_in_es: true,
+        available_in_en: true
     },
     terracota: {
         codes: ['C89C7D'],
         es: 'Terracota',
         en: 'Terra cotta',
         es_tip: 'Naranja rojizo',
-        en_tip: 'Reddish orange'
+        en_tip: 'Reddish orange',
+        available_in_es: true,
+        available_in_en: true
     },
     ante: {
         codes: ['BF8F4E'],
         es: 'Ante',
         en: 'Buff',
         es_tip: 'Marrón textil',
-        en_tip: 'Textil brown'
+        en_tip: 'Textil brown',
+        available_in_es: true,
+        available_in_en: true
     },
     cobrizo: {
         codes: ['CB6D51'],
         es: 'Cobrizo',
         en: 'Copper',
         es_tip: 'Color cobre',
-        en_tip: 'Reddish brown'
+        en_tip: 'Reddish brown',
+        available_in_es: true,
+        available_in_en: true
     },
     herrumbre: {
         codes: ['A2522B'],
         es: 'Herrumbre',
         en: 'Rust',
         es_tip: 'Hierro oxidado',
-        en_tip: 'Iron oxide, orange-red-brown'
+        en_tip: 'Iron oxide, orange-red-brown',
+        available_in_es: true,
+        available_in_en: true
     },
     siena: {
         codes: ['882D17'],
         es: 'Siena',
         en: 'Sienna',
         es_tip: 'Marrón rojizo pintura rupestre',
-        en_tip: 'Cave paintings reddish brown'
+        en_tip: 'Cave paintings reddish brown',
+        available_in_es: true,
+        available_in_en: true
     },
     borgoña: {
         codes: ['673147'],
         es: 'Borgoña',
         en: 'Burgundy',
         es_tip: 'Color del vino',
-        en_tip: 'Wine color'
+        en_tip: 'Wine color',
+        available_in_es: true,
+        available_in_en: true
     },
     violin: {
         codes: ['A10684'],
         es: 'Violín',
         en: 'Violine',
         es_tip: 'Violeta purpureo',
-        en_tip: 'Purple violet'
+        en_tip: 'Purple violet',
+        available_in_es: true,
+        available_in_en: true
     },
     malva: {
         codes: ['E0B0FF'],
         es: 'Malva',
         en: 'Mauve',
         es_tip: 'Purpura brillante',
-        en_tip: 'Brilliant purple'
+        en_tip: 'Brilliant purple',
+        available_in_es: true,
+        available_in_en: true
+    },
+    coral: {
+        codes: ['E4717A'],
+        es: 'Coral',
+        en: 'Coral',
+        es_tip: 'Rojo claro vivo',
+        en_tip: 'Alive light red',
+        available_in_es: true,
+        available_in_en: true
     },
     amaranto: {
         codes: ['FF355E'],
         es: 'Amaranto',
         en: 'Amaranth',
         es_tip: 'Rosa rojizo',
-        en_tip: 'Reddish pink'
+        en_tip: 'Reddish pink',
+        available_in_es: true,
+        available_in_en: true
     },
     rubí: {
         codes: ['E0115F'],
         es: 'Rubí',
         en: 'Ruby',
         es_tip: 'Rojo oscuro vivo',
-        en_tip: 'Vivid dark red'
+        en_tip: 'Vivid dark red',
+        available_in_es: true,
+        available_in_en: true
     },
     escarlata: {
         codes: ['ff0040'],
         es: 'Escarlata',
         en: 'Scarlet',
         es_tip: 'Rojo brillante',
-        en_tip: 'Bright red'
+        en_tip: 'Bright red',
+        available_in_es: true,
+        available_in_en: true
     },
     gules: {
         codes: ['ed1c24 '],
         es: 'Gulés',
         en: 'Gules',
         es_tip: 'Rojo heráldico, intenso',
-        en_tip: 'Heraldic red, intense'
+        en_tip: 'Heraldic red, intense',
+        available_in_es: true,
+        available_in_en: true
     },
     bermellon: {
         codes: ['E62E00'],
         es: 'Bermellón',
         en: 'Vermilion',
         es_tip: 'Rojo con toque naranja',
-        en_tip: 'Orange-coloured red'
+        en_tip: 'Orange-coloured red',
+        available_in_es: true,
+        available_in_en: true
     },
     carmesí: {
         codes: ['A51C30'],
         es: 'Carmesí',
         en: 'Crimson',
         es_tip: 'Rojo fuerte purpureo ',
-        en_tip: 'Strong purplish red'
+        en_tip: 'Strong purplish red',
+        available_in_es: true,
+        available_in_en: true
     },
     carmín: {
         codes: ['960018'],
         es: 'Carmin',
         en: 'Carmine',
         es_tip: 'Rojo oscuro levemente purpureo',
-        en_tip: 'Slightly purplish dark red'
+        en_tip: 'Slightly purplish dark red',
+        available_in_es: true,
+        available_in_en: true
     },
     caoba: {
         codes: ['C04000'],
         es: 'Caoba',
         en: 'Mahogany ',
         es_tip: 'Rojo madera',
-        en_tip: 'Wooden red'
+        en_tip: 'Wooden red',
+        available_in_es: true,
+        available_in_en: true
     },
     cardenal: {
         codes: ['C41E3A'],
         es: 'Cardenal',
         en: 'Cardinal',
         es_tip: 'Rojo vivo, toque rosa',
-        en_tip: 'Alive red, a touch of pink'
+        en_tip: 'Alive red, a touch of pink',
+        available_in_es: true,
+        available_in_en: true
     },
     granate: {
         codes: ['ab2a3e'],
         es: 'Granate',
         en: 'Maroon',
         es_tip: 'Rojo oscuro de la granada',
-        en_tip: 'Dark red from pomegranate'
+        en_tip: 'Dark red from pomegranate',
+        available_in_es: true,
+        available_in_en: true
+    },
+    añil: {
+        codes: ['264796'],
+        es: 'Añil',
+        en: 'Indigo',
+        es_tip: 'Azul oscuro y profundo. Índigo ó Glasto',
+        en_tip: 'Deep dark blue',
+        available_in_es: true,
+        available_in_en: true
+    },
+    indigo: {
+        codes: ['264796'],
+        es: 'Índigo',
+        en: 'Indigo',
+        es_tip: 'Azul oscuro y profundo. Añil ó Glasto',
+        en_tip: 'Deep dark blue',
+        available_in_es: true,
+        available_in_en: false
+    },
+    glasto: {
+        codes: ['264796'],
+        es: 'Glasto',
+        en: 'Indigo',
+        es_tip: 'Azul oscuro y profundo. Índigo ó Añil',
+        en_tip: 'Deep dark blue',
+        available_in_es: true,
+        available_in_en: false
     },
 }
