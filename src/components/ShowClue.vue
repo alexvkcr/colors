f4c3edbc7554f60cafe0118de81689a966a83105
Alejandro Díaz <alexvkcr@gmail.com>
2020-04-26 22:30:06 +0200

    feat/new file to show the clue to help in the game

---
+++ src/components/ShowClue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ShowClue">
+    
+  </div>
+</template>
+
+
+<i18n>
+{
+  "es": {
+  },
+  "en": {
+  }
+}
+</i18n>
+
+<script>
+export default {
+  name: 'ShowClue',
+}
+</script>
+
+<style scoped>
+</style>
